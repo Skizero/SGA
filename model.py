948bc0586588163e24363049e370c30b101a9e0b
KyS <112851256+Skizero@users.noreply.github.com>
2023-04-17 11:34:11 +0900

    Update model.py

--- model.py
+++ model.py
@@ -5,32 +5,28 @@ from torch_geometric.nn.conv import MessagePassing
 
 class SGA(MessagePassing):
     def __init__(self, g):
-        super().__init__(node_dim=1, aggr='add')
+        super().__init__(node_dim=0, aggr='add')
 
-        self.a = nn.Parameter(torch.empty(g.H, g.F, 1))
-        self.W = nn.Parameter(torch.empty(g.H, g.F, g.C))
-        self.c = nn.Parameter(torch.zeros(g.H, g.K+1, 1, 1))
-
-        for i in range(g.H):
-            nn.init.xavier_uniform_(self.a[i])
-            nn.init.xavier_uniform_(self.W[i])
+        self.att = nn.Linear(g.F, g.H, bias=False)
+        self.lin = nn.Linear(g.F, g.H*g.C, bias=False)
+        self.coef = nn.Parameter(torch.zeros(g.K+1, 1, g.H, 1))
 
     def forward(self, g):
-        a = torch.matmul(g.x, self.a)/g.T
+        a = self.att(g.x)/g.T
         a = self.edge_updater(g.edge_index, a=a)
 
-        x = torch.matmul(g.x, self.W)
+        x = self.lin(g.x).view(-1, g.H, g.C)
         xs = [x]
         for _ in range(g.K):
             x = self.propagate(g.edge_index, x=x, a=a)
             xs.append(x)
 
-        x = torch.stack(xs, 1)
-        x = (self.c*x).mean((0, 1))
+        x = torch.stack(xs)
+        x = (self.coef*x).mean((0, 2))
         return x
 
     def message(self, x_j, a):
-        return a * x_j
+        return a.unsqueeze(-1) * x_j
 
     def edge_update(self, a_j, index):
-        return softmax(a_j, index, dim=1)
+        return softmax(a_j, index)
